1fb3419c4c1a863879e88d30083080cdabe5518e
Dacharla, Ravindra <ravindra.dacharla@accenture.com>
2026-07-29 06:56:13 +0000

    Updated variables.tf

--- eastus2/002-001-sample-service/infra/variables.tf
+++ eastus2/002-001-sample-service/infra/variables.tf
@@ -172,6 +172,12 @@ variable "HARNESS_DB_USE_MANAGED_IDENTITY" {
   default     = "false"
 }
 
+variable "HARNESS_PG_USE_MANAGED_IDENTITY" {
+  description = "Enable Entra ID token auth for PostgreSQL"
+  type        = string
+  default     = "false"
+}
+
 variable "harness_master_key" {
   description = "Fernet key for per-credential encryption of stored authDescriptors "
   type        = string
